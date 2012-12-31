8d051f345eaa813979c727066422d23235ef3ef5
rfsheffer@gmail.com <rfsheffer@gmail.com@426e1742-4a04-eb04-0dee-b965d650e3a2>
2012-12-31 20:11:14 +0000

    - Client / Server structure

---
+++ trunk/server/server_entity.h
@@ -0,0 +1,14 @@
+//
+//  server_entity.h
+//  server
+//
+//  Created by Ryan Sheffer on 2012-12-31.
+//  Copyright (c) 2012 Ryan Sheffer. All rights reserved.
+//
+
+#ifndef __server__server_entity__
+#define __server__server_entity__
+
+
+
+#endif /* defined(__server__server_entity__) */
