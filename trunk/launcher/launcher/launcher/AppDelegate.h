80523c54150031cb4f81f4ed3be3a0b4bc1efd30
rfsheffer@gmail.com <rfsheffer@gmail.com@426e1742-4a04-eb04-0dee-b965d650e3a2>
2013-02-15 03:17:31 +0000

    - trying to fix SVN arg

---
+++ trunk/launcher/launcher/launcher/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  launcher
+//
+//  Created by Ryan Sheffer on 2013-02-14.
+//  Copyright (c) 2013 Ryan Sheffer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
