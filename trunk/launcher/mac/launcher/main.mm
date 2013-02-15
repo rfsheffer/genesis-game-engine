db8486a263fec99c24efd0890a70e742d4025760
rfsheffer@gmail.com <rfsheffer@gmail.com@426e1742-4a04-eb04-0dee-b965d650e3a2>
2013-02-15 05:38:22 +0000

    - launcher stuff

---
+++ trunk/launcher/mac/launcher/main.mm
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Main entry point for the MAC implementation of the Genesis Project
+//
+//  Created by Ryan Sheffer on 2012-10-07.
+//  Copyright (c) 2012 Ryan Sheffer. All rights reserved.
+//
+
+int main(int argc, char *argv[])
+{
+    // Start the cocoa application, see CCocoaWrapper for where the engine starts.
+    return NSApplicationMain(argc, (const char **)argv);
+}
