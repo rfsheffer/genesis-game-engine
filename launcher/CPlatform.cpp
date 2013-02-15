ed14740804612d31dbd9b9b055b7c639a77b4f14
rfsheffer@gmail.com <rfsheffer@gmail.com@426e1742-4a04-eb04-0dee-b965d650e3a2>
2013-02-15 04:43:59 +0000

    - Uploading missing files

---
+++ launcher/CPlatform.cpp
@@ -0,0 +1,27 @@
+//
+//  CPlatform.cpp
+//  Extension for accessing platform information.
+//
+//  Created by Ryan Sheffer on 2013-01-27.
+//  Copyright (c) 2013 Ryan Sheffer. All rights reserved.
+//
+
+#include "CPlatform.h"
+
+/**
+ * Initialization of the platform. This will happen after the extension
+ * manager has been created.
+ * @param pExtensions The extension manager which created this extension.
+ */
+void CPlatform::Initialize(void)
+{
+
+}
+
+/**
+ * Called usually at the end of program execution.
+ */
+void CPlatform::Shutdown(void)
+{
+    
+}
---
+++ launcher/CPlatform.h
@@ -0,0 +1,44 @@
+//
+//  CPlatform.cpp
+//  Extension for accessing platform information.
+//
+//  Created by Ryan Sheffer on 2013-01-27.
+//  Copyright (c) 2013 Ryan Sheffer. All rights reserved.
+//
+
+#ifndef CPLATFORM_H
+#define CPLATFORM_H
+
+#include "IPlatform.h"
+#include "PlatformWrapper.h"
+
+class CExtensions;
+
+/*! A class used for getting access to platform functions. */
+class CPlatform : public IPlatform
+{
+public:
+    void            Initialize(void);
+    void            Shutdown(void);
+    
+    /**
+     * Gets the state ( true[on] or false[off] of a keyboard key.
+     * @param character The key to check.
+     */
+    bool     GetKeyState(char character) const
+    {
+        return PlatformWrapper::GetKeyState(character);
+    }
+
+    /**
+     * Gets the state ( true[on] or false[off] of a mouse button.
+     * @param left If true, returns the state of the left mouse button.
+     */
+    bool     GetMouseState(bool left) const
+    {
+        return PlatformWrapper::GetMouseState(left);
+    }
+};
+
+
+#endif // CPLATFORM_H
