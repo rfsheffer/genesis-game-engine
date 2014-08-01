c7210394fea678c009a1414f30696fd001e9f6db
rfsheffer@gmail.com <rfsheffer@gmail.com@426e1742-4a04-eb04-0dee-b965d650e3a2>
2014-08-01 05:13:28 +0000

    - scripting module restructuring

---
+++ trunk/scripting/scripting_test/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  scripting_test
+//
+//  Created by Ryan Sheffer on 2014-07-31.
+//  Copyright (c) 2014 Ryan Sheffer. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+using std::string;
+
+struct Name { int hash; string text; };
+struct Primitive { Name name; };
+struct Type : Primitive { int size; };
+struct EnumConstant : Primitive { int value; };
+struct Enum : Type { EnumConstant *constants; };
+struct Field : Primitive { Type type; int offset; };
+struct Function : Primitive { Field return_parameter; Field *parameters; };
+struct Class : Type { Field *fields; Function *functions; };
+struct Namespace : Primitive { Enum *enums; Class *classes; Function *functions; };
+
+int main(int argc, const char * argv[])
+{
+    
+    return 0;
+}
+
